6ef2197433a0d53f0a1c72cd5b35e8d296b996fc
nilpeep <niluferk038@gmail.com>
2024-05-08 23:29:46 +0300

    rightbar

--- src/layout/main/index.jsx
+++ src/layout/main/index.jsx
@@ -5,11 +5,14 @@ export default function MainLayout(){
     return(
         <div className="w-[1265px] mx-auto flex">
             <Sidebar/>
+            <main className="flex-1 flex gap-[30px]">
             <main className="flex-1 border-x border-[#2f3336]">
             <Outlet/>
 
             </main>
             <Rightbar/>
+
+            </main>
         </div>
     )
 }
